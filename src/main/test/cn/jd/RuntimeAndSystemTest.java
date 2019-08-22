e97cf6a0de9f48a97761e23e9923837576c23f09
yan <732283628@qq.com>
2019-08-23 01:12:14 +0800

    System

--- src/main/test/cn/jd/RuntimeAndSystemTest.java
+++ src/main/test/cn/jd/RuntimeAndSystemTest.java
@@ -6,12 +6,13 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Properties;
 
 //@cn.jd.SpringBootTest(classes= PosApp.class,webEnvironment = cn.jd.SpringBootTest.WebEnvironment.DEFINED_PORT)
 //@RunWith(SpringRunner.class)
 //@EnableWebSocket
 //@EnableAutoConfiguration
-public class RuntimeTest {
+public class RuntimeAndSystemTest {
 
 
 	@Test
@@ -39,11 +40,22 @@ public class RuntimeTest {
 
 	@Test
 	public void testOpenFile() throws Exception {
+		Properties properties = System.getProperties();
+
 		String userDir = System.getProperty("user.dir");
 		File dir = new File(userDir);
 		
 		String s = "open "+ dir.toString();
 		Process p = Runtime.getRuntime().exec(s);
+	}
+
+	@Test
+	public void testOS() throws Exception {
+		String property = System.getProperty("os.name");
+		boolean windows = property.toLowerCase().contains("windows");
+		if (windows){
+
+		}
 	}
 
 

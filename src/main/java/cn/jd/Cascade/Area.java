fa53cd7b1196de01c5461a09122295065b448aa9
yan <732283628@qq.com>
2019-08-26 19:45:01 +0800

    省市县三级联动

--- src/main/java/cn/jd/Cascade/Area.java
+++ src/main/java/cn/jd/Cascade/Area.java
@@ -2,9 +2,15 @@ package cn.jd.Cascade;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Area {
+
     int ID;
     int TopID;
     String AddName;
+
+    //下级列表
+    List<Area> downList;
 }

92240f75576165a51a9df7f660947108ad8e21e8
yan <yilsonyan@outlook.com>
2019-09-12 13:35:25 +0800

    新建FX项目

--- src/main/java/cn/jd/jvm/JvmTest.java
+++ src/main/java/cn/jd/jvm/JvmTest.java
@@ -2,6 +2,9 @@ package cn.jd.jvm;
 
 import org.junit.Test;
 
+import java.lang.ref.SoftReference;
+import java.lang.ref.WeakReference;
+
 public class JvmTest {
 
 	// print the Current JVM Heap Size, Maximum JVM Heap Size, and Free JVM Heap Size?
@@ -18,14 +21,26 @@ public class JvmTest {
 //		System.out.println("Free JVM Heap Size:" + Runtime.getRuntime().getFreeHeapMemory());
 
 		//TODO 必须通过Runtime.getRuntime()获取对象；获取内存得方法没有get前缀
-		System.out.println("Current JVM Heap Size:" + Runtime.getRuntime().totalMemory());
-		System.out.println("Maximum JVM Heap Size:" + Runtime.getRuntime().maxMemory());
-		System.out.println("Free JVM Heap Size:" + Runtime.getRuntime().freeMemory());
+		System.out.println("Current JVM Heap Size:" + Runtime.getRuntime().totalMemory()/1024/1024);
+		System.out.println("Maximum JVM Heap Size:" + Runtime.getRuntime().maxMemory()/1024/1024);
+		System.out.println("Free JVM Heap Size:" + Runtime.getRuntime().freeMemory()/1024/1024);
 //		System.out.println("Current JVM Heap Size:"+Runtime.totalMemory());
 //		System.out.println("Maximum JVM Heap Size:"+Runtime.maxMemory());
 //		System.out.println("Free JVM Heap Size:"+Runtime.freeMemory());
 	}
 
+
+
+	@Test
+	public void test5() throws Exception {
+		String abc = new String("abc");
+		SoftReference<String> softReference = new SoftReference<>(abc);   //软引用
+
+
+		String xyz = new String("xyz");
+		WeakReference<String> weakReference = new WeakReference<>(xyz);   //软引用
+
+	}
 
 
 }

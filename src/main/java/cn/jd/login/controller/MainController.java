e4ad30eeae4c68177bf1d3995ab63b9b49e4a8fe
yan <732283628@qq.com>
2019-09-16 23:44:08 +0800

    pom

--- src/main/java/cn/jd/login/controller/MainController.java
+++ src/main/java/cn/jd/login/controller/MainController.java
@@ -1,13 +1,14 @@
 package cn.jd.login.controller;
 
 import de.felixroske.jfxsupport.AbstractFxmlView;
+import de.felixroske.jfxsupport.FXMLController;
 import de.felixroske.jfxsupport.FXMLView;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-//@FXMLController
+@FXMLController
 @FXMLView(value = "/fxml/main.fxml")
 public class MainController extends AbstractFxmlView implements Initializable {
 

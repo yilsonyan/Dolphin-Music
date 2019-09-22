09a87230fc206cab61b4b9e560db225ff68caef8
yan <732283628@qq.com>
2019-09-22 13:00:39 +0800

    登录界面成型

--- src/main/java/cn/yan/login/controller/LoginController.java
+++ src/main/java/cn/yan/login/controller/LoginController.java
@@ -12,6 +12,9 @@ import de.felixroske.jfxsupport.AbstractFxmlView;
 import de.felixroske.jfxsupport.FXMLController;
 import de.felixroske.jfxsupport.FXMLView;
 import de.felixroske.jfxsupport.GUIState;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
@@ -26,12 +29,10 @@ import org.kordamp.ikonli.fontawesome5.FontAwesomeSolid;
 import org.kordamp.ikonli.javafx.FontIcon;
 
 import javax.annotation.PostConstruct;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 @FXMLController
@@ -42,19 +43,19 @@ public class LoginController extends AbstractFxmlView implements Initializable {
 	@FXML
 	private GridPane root;
 
-    @FXML
-    private JFXTextField usernameField;
+	@FXML
+	private JFXTextField usernameField;
 
 	@FXML
 	private JFXPasswordField passwordField;
 
-    //记住按钮
-    @FXML
-    private JFXToggleButton rememberMe;
+	//记住按钮
+	@FXML
+	private JFXToggleButton rememberMe;
 
-    //登录按钮
-    @FXML
-    private JFXButton signIn;
+	//登录按钮
+	@FXML
+	private JFXButton signIn;
 
 	//注册按钮
 	@FXML
@@ -64,8 +65,8 @@ public class LoginController extends AbstractFxmlView implements Initializable {
 	@FXML
 	private Text msg;
 
-    //登录界面
-	public static Stage loginStage ;
+	//登录界面
+	public static Stage loginStage;
 	//注册界面
 	public static Stage registerStage;
 
@@ -84,24 +85,22 @@ public class LoginController extends AbstractFxmlView implements Initializable {
 		//stage.setIconified(true);
 		//设置窗口关闭按钮事件
 		loginStage.setOnCloseRequest(
-			event -> new DialogBuilder(usernameField).
-				setTitle("Oops!").
-				setMessage("Do you really want to leave me alone?").
-				setPositiveBtn("Sorry for that", ()->{}).
-				setNegativeBtn("Cancel", ()->event.consume()).
-				create()
+				event -> new DialogBuilder(usernameField).
+						setTitle("Oops!").
+						setMessage("Do you really want to leave me alone?").
+						setPositiveBtn("Sorry for that", () -> {}).
+						setNegativeBtn("Cancel", () -> event.consume()).
+						create()
 		);
 
 	}
 
 
 	@PostConstruct
-	public void init(){
+	public void init() throws Exception {
 
 		//加载样式css
 		Parent view = getView();
-
-
 		view.getStylesheets().add("css/Login.css");
 		//view.setStyle("-fx-background-image:url(static/entrance-backgroud/entrance_bg1.jpg);-fx-background-size:500px");
 		//loginIn.getStyleClass().add("button-raised");
@@ -141,6 +140,9 @@ public class LoginController extends AbstractFxmlView implements Initializable {
 		//监听键盘回车事件：登录
 		root.setOnKeyPressed(event -> enterKeyEvent(event));
 
+		//输入框绑定序列化登录信息
+		getLoginInfo();
+
 	}
 
 	//登录按钮事件
@@ -148,7 +150,7 @@ public class LoginController extends AbstractFxmlView implements Initializable {
 		//校验
 		boolean usernameValidate = usernameField.validate();
 		boolean passwordValidate = passwordField.validate();
-		if (usernameValidate && passwordValidate){
+		if (usernameValidate && passwordValidate) {
 			//登录验证
 			String userName = usernameField.getCharacters().toString();
 			String password = passwordField.getCharacters().toString();
@@ -161,8 +163,11 @@ public class LoginController extends AbstractFxmlView implements Initializable {
 
 				//判断是否勾选记住帐号和密码
 				boolean selected = rememberMe.isSelected();
-				if (selected){
-					saveLoginInfo(userName,password);
+				if (selected) {
+					saveLoginInfo(userName, password);
+				} else {
+					//删除序列化文件
+					deleteLoginInfo();
 				}
 
 				loginStage.close();
@@ -187,23 +192,23 @@ public class LoginController extends AbstractFxmlView implements Initializable {
 
 	//监听键盘回车事件：登录
 	public void enterKeyEvent(KeyEvent event) {
-		if(event.getCode() == KeyCode.ENTER){
+		if (event.getCode() == KeyCode.ENTER) {
 			signInBtn(null);
 		}
 	}
 
 
 	//序列化登录信息
-	private void saveLoginInfo(String userName,String password) {
-		new Thread(()->{
+	private void saveLoginInfo(String userName, String password) {
+		new Thread(() -> {
 			HashMap<String, String> map = new HashMap<>();
-			map.put("userName",userName);
-			map.put("password",password);
+			map.put("userName", userName);
+			map.put("password", password);
 			//序列化
-			URL url = this.getClass().getClassLoader().getResource("config");
-			File serializableFile = new File(url.getPath()+"/Serializable");
-			try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(serializableFile))){
-				if (!serializableFile.exists()){
+			URL url = this.getClass().getClassLoader().getResource("");
+			File serializableFile = new File(url.getPath() + "/user.serializable");
+			try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(serializableFile))) {
+				if (!serializableFile.exists()) {
 					serializableFile.createNewFile();
 				}
 				objectOutputStream.writeObject(map);
@@ -213,6 +218,93 @@ public class LoginController extends AbstractFxmlView implements Initializable {
 			}
 		}).start();
 
+	}
+
+
+
+	//删除序列化文件
+	public void deleteLoginInfo() {
+		//获取序列化文件
+		URL url = this.getClass().getClassLoader().getResource("");
+		File serializableFile = new File(url.getPath() + "/user.serializable");
+		try {
+			if (serializableFile.exists()) {
+				serializableFile.delete();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+
+	//输入框绑定序列化登录信息
+	private void getLoginInfo() throws Exception{
+		Map<String, String> loginInfo;
+		//获取序列化文件
+		URL url = this.getClass().getClassLoader().getResource("");
+		File serializableFile = new File(url.getPath() + "/user.serializable");
+		if (!serializableFile.exists()) {
+			return;
+		}else {
+			ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(serializableFile));
+			loginInfo = (Map<String, String>) objectInputStream.readObject();
+			objectInputStream.close();
+		}
+
+		Service<String> readUserNameService = new Service<String>() {
+			@Override
+			protected Task<String> createTask() {
+				return new Task<String>() {
+					@Override
+					protected String call() {
+						String userName = "";
+						//Map<String, String> loginInfo = readLoginInfo();
+						if (loginInfo != null){
+							userName = loginInfo.get("userName");
+						}
+						return userName;
+					}
+				};
+			}
+		};
+
+		Service<String> readPwdService = new Service<String>() {
+			@Override
+			protected Task<String> createTask() {
+				return new Task<String>() {
+					@Override
+					protected String call() {
+						String password = "";
+						//Map<String, String> loginInfo = readLoginInfo();
+						if (loginInfo != null){
+							password = loginInfo.get("password");
+						}
+						return password;
+					}
+				};
+			}
+		};
+
+		//绑定text属性为service的text属性
+		usernameField.textProperty().bind(readUserNameService.valueProperty());
+		passwordField.textProperty().bind(readPwdService.valueProperty());
+
+		//任务完成时会调用：界面绑定写入信息后，解绑，否则界面上不能修改输入的内容
+		readUserNameService.setOnSucceeded((WorkerStateEvent stateEvent) -> {
+			usernameField.textProperty().unbind();
+		});
+		//任务完成时会调用
+		readPwdService.setOnSucceeded((WorkerStateEvent stateEvent) -> {
+			passwordField.textProperty().unbind();
+		});
+
+		//设置记住按钮为勾选状态
+		rememberMe.setSelected(true);
+
+		//启动任务start()一定是最后才调用的
+		readUserNameService.start();
+		readPwdService.start();
 	}
 
 
--- src/main/java/cn/yan/login/stage/RegisterStage.java
+++ src/main/java/cn/yan/login/stage/RegisterStage.java
@@ -1,11 +1,13 @@
 package cn.yan.login.stage;
 
 import cn.yan.login.config.SceneConfig;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.io.IOException;
 import java.net.URL;
@@ -14,18 +16,54 @@ public class RegisterStage extends Stage{
 
 	{
 		URL url = getClass().getClassLoader().getResource("fxml/register.fxml");
-		Parent root = null;
+		AnchorPane root = null;
 		try {
 			root = FXMLLoader.load(url);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		//添加css
+
 		Scene scene = new Scene(root, SceneConfig.width, SceneConfig.height);
-		this.setTitle("注册新用户");
-		//loginStage.setFullScreen(true);
+		//scene.getStylesheets().add(this.getClass().getClassLoader().getResource("css/Login.css").toExternalForm());
+		scene.getStylesheets().add("css/Login.css");
+
+
+
 		this.setScene(scene);
-		this.getIcons().add(new Image("static/icon.png"));
+		this.setTitle("Dolphin Music");
+		//this.setFullScreen(true);
+		this.setResizable(false);//设置不可调整大小
+		//设置窗口的图标
+		this.getIcons().add(new Image("/static/icon.png"));
 		this.show();
+
+
+		//关闭事件
+		this.setOnCloseRequest(new EventHandler<WindowEvent>() {
+			@Override
+			public void handle(WindowEvent event) {
+
+				//对话框 Alert Alert.AlertType.CONFIRMATION：反问对话框
+				/*
+			    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+			    //设置对话框标题
+			    alert.setTitle("Exit");
+			    //设置内容
+			    alert.setHeaderText("Are you sure to exit");
+			    //显示对话框
+			    Optional<ButtonType> result = alert.showAndWait();
+			    //如果点击OK
+			    if (result.get() == ButtonType.OK){
+				    // ... user chose OK
+				    System.exit(0);
+				    //否则
+			    } else {
+				    event.consume();
+			    }
+				*/
+			}
+		});
 	}
 
 
